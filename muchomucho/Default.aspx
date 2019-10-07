061adf8d1f5c9eb522d454733d35cf4829f9b842
Emmanuel Austria <Emmanuel Austria@DESKTOP-LEQCKKH>
2019-10-07 19:25:45 +0800

    Add project files.

---
+++ muchomucho/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="muchomucho.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>AVLINC WEBSITE</title>
+
+    <link rel="stylesheet" href="Content/bootstrap.min.css" />
+    <script src="scripts/bootstrap.min.js" type="text/javascript"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <center><h1>Ardee's Ventures and Logistics, Inc.</h1></center>
+        <div class="container">
+            <div>
+                <label class="lbl"><strong>Environment</strong></label></div>
+            <div>
+                <asp:DropDownList ID="ddlType" CssClass="form-control" runat="server">
+                    <asp:ListItem Selected="True" Value="0">Test</asp:ListItem>
+                    <asp:ListItem Value="1">Production</asp:ListItem>
+                </asp:DropDownList>
+                <div>
+                    <label class="lbl"><strong>System</strong></label></div>
+            </div>
+            <div>
+                <asp:DropDownList ID="ddlSystem" CssClass="form-control" runat="server">
+                    <asp:ListItem Selected="True" Value="0">Payroll</asp:ListItem>
+                    <asp:ListItem Value="1">Accounting</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+            <br />
+            <div>
+                <asp:Button CssClass="btn btn-primary" ID="btnLaunch" runat="server" Text="Launch System" OnClick="btnLaunch_Click" />
+            </div>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ muchomucho/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace muchomucho
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLaunch_Click(object sender, EventArgs e)
+        {
+            //TEST PAYROLL
+            if (ddlType.SelectedIndex==0 && ddlSystem.SelectedIndex == 0)
+            {
+                string newWindowUrl = "https://avlincpayroll-test.reduixph.com";
+                ClientScript.RegisterStartupScript(this.GetType(), "key", "<script>window.open('" + newWindowUrl + "');</script>");
+            }
+            //TEST ACCTG
+            else if (ddlType.SelectedIndex == 0 && ddlSystem.SelectedIndex == 1)
+            {
+                string newWindowUrl = "https://avlincacctg-test.reduixph.com";
+                ClientScript.RegisterStartupScript(this.GetType(), "key", "<script>window.open('" + newWindowUrl + "');</script>");
+            }
+            //PROD PAYROLL
+            else if (ddlType.SelectedIndex == 1 && ddlSystem.SelectedIndex == 0)
+            {
+                string newWindowUrl = "https://avlincpayroll.reduixph.com";
+                ClientScript.RegisterStartupScript(this.GetType(), "key", "<script>window.open('" + newWindowUrl + "');</script>");
+            }
+            //PROD ACCTG
+            else if (ddlType.SelectedIndex == 1 && ddlSystem.SelectedIndex == 1)
+            {
+                string newWindowUrl = "https://avlincacctg.reduixph.com";
+                ClientScript.RegisterStartupScript(this.GetType(), "key", "<script>window.open('" + newWindowUrl + "');</script>");
+            }
+        }
+    }
+}
